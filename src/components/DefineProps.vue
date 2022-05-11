78a31240b676c9a3045a61456a764e9c91e4c07f
吴琛 <wuchen@xhd.cn>
2022-05-11 16:58:06 +0800

    首次提交

---
+++ src/components/DefineProps.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    msg?:string,
+    ageList:Array<number>
+  }>(),
+    {msg:'hello',
+    ageList:()=>[1,2]
+    }
+)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h1>{{ ageList }}</h1>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
